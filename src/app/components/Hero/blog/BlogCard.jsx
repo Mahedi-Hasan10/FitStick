b7295811b50693fde172e99b0a0d6cc3a8aaac7c
Mahedi-Hasan10 <hasanmdmahedi193@gmail.com>
2024-02-04 12:11:55 +0600

    blog done

--- src/app/components/Hero/blog/BlogCard.jsx
+++ src/app/components/Hero/blog/BlogCard.jsx
@@ -1,8 +1,21 @@
+import Link from 'next/link';
 import Image from 'next/image';
-const BlogCard = () => {
+const BlogCard = ({thumbnail, subTitle,Title}) => {
     return (
-        <div>
-            <Image src='/blog/b1.png' alt='blog1' width={424} height={280}/>;
+        <div className='group transition-all duration-500 cursor-pointer w-fit max-w-[424px]'>
+            <Image src={thumbnail} alt='blog1' width={424} height={280}/>
+            <div className='p-4 border-l border-opacity-10 group-hover:border-opacity-20 transition duration-300 border-l-white border-r border-r-white border-b border-b-white'>
+                <div className='text-white text-lg font-bold bottom-card flex items-center gap-3 mt-[10px]'>
+                    <div className='h-[1px] bg-[#FF4E25] bg-opacity-60 w-[42px] group-hover:bg-opacity-100'></div>
+                    <Link className='text-[#FF4E25] text-base font-semibold text-opacity-60 group-hover:text-opacity-100' href="/">{subTitle}</Link>
+                </div>
+
+                <h5 className='text-[28px] font-bold leading-[44px] capitalize group-hover:text-[#FF4E25] transition duration-300 line-clamp-2'>{Title}</h5>
+                <div className='text-white text-lg font-bold bottom-card flex items-center gap-5 mt-[10px]'>
+                    <div className='h-[14px] w-[14px] bg-[#ff4e25] rounded-full group-hover:rounded-none group-hover:h-[1px] group-hover:w-[42px] transition-all duration-500'></div>
+                    <Link className='group-hover:opacity-100 opacity-50 text-lg font-bold' href="/">Read More</Link>
+                </div>
+            </div>
         </div>
     );
 };
--- src/app/components/Hero/blog/LatestBlog.jsx
+++ src/app/components/Hero/blog/LatestBlog.jsx
@@ -8,8 +8,10 @@ const LatestBlog = () => {
                 <h2 className='xl:text-5xl lg:text-3xl text-2xl 2xl:font-bold lg:font-semibold font-medium lg:leading-[57px] leading-[30px]'>Latest Blog Posts</h2>
                 <Link href="/" className='lg:text-lg text-base lg:font-bold font-medium text-[#FF4E25] underline'>View All Blogs</Link>
             </div>
-            <div>
-                <BlogCard/>
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-3' >
+                <BlogCard thumbnail='/blog/b1.png' subTitle="Fitness" Title="fitness helps you think and fell better"/>
+                <BlogCard thumbnail='/blog/b2.png' subTitle="ACROBATICS" Title="10 tips how to prepare me fast and easy"/>
+                <BlogCard thumbnail='/blog/b3.png' subTitle="Fitness" Title="today is the best day to in training"/>
             </div>
         </section>
     );
